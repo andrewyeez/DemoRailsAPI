67d3683faf8762c973892172ca5fb40c31a56ef1
Andrew Yee <andrewyee18331@sbcglobal.net>
2016-04-10 15:32:01 -0700

    start api work on fruits table

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   ###################################################################
 
   # will take me to a controller named 'test_controller' that will call on the method 'index' render an erb file
-  root to: 'test#index' 
-  
+  root to: 'landing_page#index' 
+
+  # fruits routes API 
+  match '/fruits' => 'fruits#all', via :get
+  match '/fruits/:id' =>'fruits#single', via :get
+  match '/fruits/search/description/:description', via :get
+  match '/fruits/search/name/:color', via :get
+
 end
